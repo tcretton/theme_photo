a203a0268093e3c4457f77a9bf565cb8fcd82e96
Tércio Cretton <tcretton@users.noreply.github.com>
2021-01-27 18:36:24 -0300

    Update config.php

--- config.php
+++ config.php
@@ -27,6 +27,7 @@ $THEME->parents = ['boost'];
 // A dock is a way to take blocks out of the page and put them in a persistent floating area on the side of the page. Boost does not support a dock so we won't either - but look at bootstrapbase for an example of a theme with a dock.
 $THEME->enable_dock = false;
 
+// This is an old setting used to load specific CSS for some YUI JS. We don't need it in Boost based themes because Boost provides default styling for the YUI modules that we use. It is not recommended to use this setting anymore.
 $THEME->yuicssmodules = array();
 
 // Most themes will use this rendererfactory as this is the one that allows the theme to override any other renderer.
@@ -35,6 +36,9 @@ $THEME->rendererfactory = 'theme_overridden_renderer_factory';
 // This is a list of blocks that are required to exist on all pages for this theme to function correctly. For example bootstrap base requires the settings and navigation blocks because otherwise there would be no way to navigate to all the pages in Moodle. Boost does not require these blocks because it provides other ways to navigate built into the theme.
 $THEME->requiredblocks = '';
 
+// This is a feature that tells the blocks library not to use the "Add a block" block. We don't want this in boost based themes because it forces a block region into the page when editing is enabled and it takes up too much room.
 $THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
+// This is the function that returns the SCSS source for the main file in our theme. We override the boost version because we want to allow presets uploaded to our own theme file area to be selected in the preset list.
 $THEME->scss = function($theme) {
     return theme_photo_get_main_scss_content($theme);
+};
