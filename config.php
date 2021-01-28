ea39e4b57c8480614b9a5e34105a625d685131c2
Tércio Cretton <tcretton@users.noreply.github.com>
2021-01-28 08:24:46 -0300

    Update config.php

--- config.php
+++ config.php
@@ -26,9 +26,11 @@ $THEME->name = 'photo';
 $THEME->sheets = [];
 
 // This is a setting that can be used to provide some styling to the content in the TinyMCE text editor. This is no longer the default text editor and "Atto" does not need this setting so we won't provide anything. If we did it would work the same as the previous setting - listing a file in the /styles/ folder.
+// Esta é uma configuração que pode ser usada para fornecer algum estilo ao conteúdo no editor de texto TinyMCE. Este não é mais o editor de texto padrão e "Atto" não precisa dessa configuração, portanto, não forneceremos nada. Se fizéssemos, funcionaria da mesma forma que a configuração anterior - listar um arquivo na pasta / styles /.
 $THEME->editor_sheets = [];
 
 // This is a critical setting. We want to inherit from theme_boost because it provides a great starting point for SCSS bootstrap4 themes. We could add more than one parent here to inherit from multiple parents, and if we did they would be processed in order of importance (later themes overriding earlier ones). Things we will inherit from the parent theme include styles and mustache templates and some (not all) settings.
+// Este é um cenário crítico. Queremos herdar de theme_boost porque ele fornece um ótimo ponto de partida para temas SCSS bootstrap4. Poderíamos adicionar mais de um pai aqui para herdar de vários pais e, se o fizéssemos, eles seriam processados em ordem de importância (temas posteriores substituindo os anteriores). As coisas que herdaremos do tema pai incluem estilos e modelos de bigode e algumas (não todas) configurações.
 $THEME->parents = ['boost'];
 
 // A dock is a way to take blocks out of the page and put them in a persistent floating area on the side of the page. Boost does not support a dock so we won't either - but look at bootstrapbase for an example of a theme with a dock.
@@ -44,7 +46,9 @@ $THEME->rendererfactory = 'theme_overridden_renderer_factory';
 $THEME->requiredblocks = '';
 
 // This is a feature that tells the blocks library not to use the "Add a block" block. We don't want this in boost based themes because it forces a block region into the page when editing is enabled and it takes up too much room.
+// Este é um recurso que diz à biblioteca de blocos para não usar o bloco "Adicionar um bloco". Não queremos isso em temas baseados em boost porque força uma região de bloco na página quando a edição está habilitada e ocupa muito espaço.
 $THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
+
 // This is the function that returns the SCSS source for the main file in our theme. We override the boost version because we want to allow presets uploaded to our own theme file area to be selected in the preset list.
 $THEME->scss = function($theme) {
     return theme_photo_get_main_scss_content($theme);
